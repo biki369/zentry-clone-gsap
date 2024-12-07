480e0fe58eead91f5926f25b4c9d7574baa47388
biki369 <bikitirua321@gmail.com>
2024-12-07 18:52:13 +0530

    UTD 7:12:24::17:52

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,7 +9,7 @@ import gsap from "gsap";
 const Hero = () => {
   const [currentIndex, setCurrentIndex] = useState(1);
   const [hasClick, setHasClick] = useState(false);
-  const [isPlaying, setIsPlaying] = useState(false);
+  // const [isPlaying, setIsPlaying] = useState(false);
   const [isLoading, setLoading] = useState(false);
   const [loadedVideo, setLoadedVideo] = useState(0);
 
@@ -150,7 +150,7 @@ const Hero = () => {
           G<b>a</b>ming
         </h1>
         <div className="absolute left-0 top-0 z-40 size-full">
-          <div className="mt-23 px-5 sm:px-10">
+          <div className="mt-24 px-5 sm:px-10">
             <h1 className="special-font hero-heading text-blue-100">redfie</h1>
             <p className="mb-5 max-w-64 font-robert-regular text-blue-100">
               Enter the Metagame Layer <br/>
