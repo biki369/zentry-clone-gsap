77d1347e1341b7f6a7e8235afc56c277c17672f7
biki369 <bikitirua321@gmail.com>
2024-12-09 17:43:55 +0530

    UTD 8:12:24::17:43

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,17 @@ import './App.css'
 import Hero from './components/Hero'
 import About from './components/About'
 import Navbar from './components/Navbar'
+import Feature from './components/Feature'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <main className=' relative min-h-screen overflow-x-hidden w-screen'>
       <Navbar/>
       <Hero />
       <About/>
+      <Feature/>
     </main>
   )
 }
